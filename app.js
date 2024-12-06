4831767463b72a742014def000253dea400c1661
hayde26904 <hayde26904@live.ytech.edu>
2024-12-06 14:57:49 -0500

    login button worky

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.listen(3000, () => {
     console.log('Server started on PORT 3000.')
 });
