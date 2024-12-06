eca7e9eb2c29a5b79ffb4470f89400d7545aa3bb
Conno26678 <conno26678@live.ytech.edu>
2024-12-06 14:47:57 -0500

    Da set-up

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require ('express');
+const app = express()
+const port = 3000;
 
-const app = express();
-const PORT = 3000;
+app.set('view engine', 'ejs');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// Routes
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-// Start server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log('Server started on PORT 3000.')
 });
